2e49d7418549b354c0b48aa7275ef891cc96f1b6
TheSecretIron <70634067+TheSecretIron@users.noreply.github.com>
2020-10-04 15:14:07 +0800

    Add files via upload

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,44 +1,218 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class noti extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Info'),
-          centerTitle: true,
-        ),
-        body: BodyLayout(),
+      appBar: AppBar(
+        title: Text('Info'),
+        centerTitle: true,
+      ),
+      body: BodyLayout(),
     );
   }
 }
 
-class BodyLayout extends StatelessWidget {
+class BodyLayout extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return _myListView(context);
-  }
+  _BodyLayoutState createState() => _BodyLayoutState();
 }
 
-// replace this function with the code in the examples
-Widget _myListView(BuildContext context) {
+class _BodyLayoutState extends State<BodyLayout> {
 
-  final titles = ['Notify me about new red zone areas', 'Notify me about new daily cases', 'Notify me about new daily recovered',
-    'Remind me to wear a mask ', 'Remind me to wash my hand', 'Remind me to drink water',];
+  bool status1 = false;
+  bool status2 = false;
+  bool status3 = false;
+  bool status4 = false;
+  bool status5 = false;
+  bool status6 = false;
 
-  final icons = [Icons.notification_important, Icons.directions_boat,
-    Icons.directions_bus, Icons.face, Icons.fingerprint,
-    Icons.bubble_chart,];
-
-  return ListView.builder(
-    itemCount: titles.length,
-    itemBuilder: (context, index) {
-      return Card( //                           <-- Card widget
-        child: ListTile(
-          leading: Icon(icons[index]),
-          title: Text(titles[index]),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.notification_important),
+              Text('Notify me about new red zone areas'),
+              FlutterSwitch(
+                value: status1,
+                onToggle: (val) {
+                  setState(() {
+                    status1 = val;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
-      );
-    },
-  );
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.directions_boat,),
+              Text('Notify me about new daily cases',),
+              FlutterSwitch(
+                value: status2,
+                onToggle: (val) {
+                  setState(() {
+                    status2 = val;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.directions_bus,),
+              Text('Notify me about new daily recovered',),
+              FlutterSwitch(
+                value: status3,
+                onToggle: (val) {
+                  setState(() {
+                    status3 = val;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.face,),
+              Text('Remind me to wear a mask ',),
+              FlutterSwitch(
+                value: status4,
+                onToggle: (val) {
+                  setState(() {
+                    status4 = val;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.fingerprint,),
+              Text('Remind me to wash my hand',),
+              FlutterSwitch(
+                value: status5,
+                onToggle: (val) {
+                  setState(() {
+                    status5 = val;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          padding: EdgeInsets.all(15.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.bubble_chart,),
+              Text('Remind me to drink water',),
+              FlutterSwitch(
+                value: status6,
+                onToggle: (val) {
+                  setState(() {
+                    status6 = val;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
